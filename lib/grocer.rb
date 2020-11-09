86eba16648e3d4d5f39036852862448ee932b655
Avelon Pang <avelonpang@gmail.com>
2020-11-09 19:44:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,7 +13,7 @@ end
 def find_item_by_name_in_collection(name, collection)
   counter = 0 
   while counter < collection.length 
-  
+  if collection[counter][:item] == 
  
 
 def consolidate_cart(cart)
