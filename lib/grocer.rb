32282c5221b8d1ec6540ce9805f1e7cba9d82663
Avelon Pang <avelonpang@gmail.com>
2020-11-09 19:46:38 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,14 +1,14 @@
 
 require 'pry'
 
-def find_item_by_name_in_collection(name, collection)
-  collection.each do |item_info|
-    if item_info[:item] == name
-      return item_info
-    end 
-  end 
-  nil
-end 
+# def find_item_by_name_in_collection(name, collection)
+#   collection.each do |item_info|
+#     if item_info[:item] == name
+#       return item_info
+#     end 
+#   end 
+#   nil
+# end 
 
 def find_item_by_name_in_collection(name, collection)
   counter = 0 
